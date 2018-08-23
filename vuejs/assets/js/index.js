7744b5b92607af39b7df0f481b1b230b318f2e27
Ugo <ugo.arzur@morning.com>
2018-08-23 13:43:19 +0200

    rename dinosaurus list and add filters to the vuejs component and in the template

--- vuejs/assets/js/index.js
+++ vuejs/assets/js/index.js
@@ -8,10 +8,10 @@ var card = new Vue({
     el: "#card",
     data: {
         title: "Dinosaurs",
-        items: [
-            { text: "Velociraptor" },
-            { text: "Triceratops" },
-            { text: "Brutosaurus" },
+        dinos: [
+            { name: "Velociraptor", weight: "15 kg" },
+            { name: "Triceratops", weight: "6.000 kg" },
+            { name: "Stegosaurus", weight: "2.500 kg" },
         ],
         description: "<strong>Dinosaurs</strong> are very diverse group of animals of the clade <em>Dinosauria</em> that first appeared during the Triassic period."
     },
@@ -19,12 +19,28 @@ var card = new Vue({
         addItem: function() {
             var input = document.getElementById('itemForm');
             if(input.value !== ''){
-                this.items.push({ text: input.value })
+                this.dinos.push({ name: input.value })
                 input.value = "";
             }
         },
         deleteItem: function(index) {
-             this.items.splice(index, 1)
+             this.dinos.splice(index, 1)
+        }
+    },
+    filters: {
+        capitalize: function(value){
+            if(!value) return '';
+            value.toString();
+            return value.charAt(0).toUpperCase() + value.slice(1);
+        },
+        undercase: function(value){
+            if(!value) return '';
+                value.toString();
+                return value.toLowerCase();
+        },
+        url: function(value){
+            value = value.toString();
+            return "http://en.wikipedia.org/wiki/" + value;
         }
     }
 })
