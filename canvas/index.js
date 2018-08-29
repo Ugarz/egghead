dbaf1412ac480dd1cfacbbb815326ac05f6721f0
Ugo <ugo.arzur@morning.com>
2018-08-29 14:26:28 +0200

    Episode 7 - Add click interaction to HTML Canvas using JavaScript events

--- canvas/index.js
+++ canvas/index.js
@@ -1,41 +1,23 @@
-// Get the canvas
 const canvas = document.querySelector('#canvas')
-canvas.width = canvas.height = 500;
-
-// Get the 2d context canvas
 const ctx = canvas.getContext('2d')
+const coords = document.querySelector('.display-coords')
 
-ctx.beginPath();
-ctx.moveTo(100, 70) // move to the spot
-ctx.lineTo(100, 20) // draw a vertical line
-ctx.arcTo(150, 20, 150, 70, 50)
-ctx.lineTo(100, 70) // draw a horizontal line
+console.log('Living ma life like its Golden');
+ctx.fillStyle = "gold"
 
-ctx.fill()
-ctx.closePath()
+console.log('Filling rectangle');
+ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
 
-// var i = 0;
-// function loopIt() {
-//     if (i++ < Math.PI * 2) {
-//         drawPurpleCircle(i);
-//     } else {
-//         i = 0;
-//         ctx.clearRect(0, 0, 500, 500);
-//         setTimeout(loopIt, 400);
-//     }
-// };
-// loopIt();
+/**
+ * G
+ * @param {Object} event 
+ */
+const getCoords = (event) => {
+    const container = canvas.getBoundingClientRect();
+    const x = (event.clientX - container.left) - container.width / 2;
+    const y = (event.clientY - container.top) - container.height / 2;
+    console.log('Coords', {x,y})
+    coords.textContent = `${x}, ${y}`
+};
 
-
-// function drawPurpleCircle(i) {
-//     ctx.beginPath();
-//     ctx.arc(100, 70, 50, 0, i);
-//     ctx.fillStyle = "rebeccaPurple";
-//     ctx.fill();
-//     ctx.closePath();
-//     setTimeout(loopIt, 400);
-// };
-
-// Notes
-// 1 radian = 57.2957795 degrees
-// var radians = (Math.PI/180) * degrees
+canvas.addEventListener('click', getCoords)
