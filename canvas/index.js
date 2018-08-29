7d09e188f62f94b7b22bc98eb06c4d93e460129e
Ugo <ugo.arzur@morning.com>
2018-08-29 13:42:41 +0200

    Episode 5 - Add strokes to circles using HTML Canvas

--- canvas/index.js
+++ canvas/index.js
@@ -5,23 +5,22 @@ canvas.width = canvas.height = 500;
 // Get the 2d context canvas
 const ctx = canvas.getContext('2d')
 
-// // Get the 2d context canvas
-// ctx.fillStyle = 'gold'
-// // ctx.fillStyle = "rgb(255, 192, 203)"
-// // ctx.fillStyle = "rgba(255, 192, 203, .5)"
-
-// ctx.fillStyle = "#ffc0cb"
-// ctx.fillRect(0, 0, 50, 50)
-
-// Episode 4 -  Arc
+// Simple arc filled
 ctx.beginPath();
-// ctx.arc(centerX, centerY, radius, startAngle, endAngle, isAntiClockwise)
-
 ctx.arc(50, 50, 20, Math.PI/180 * 0, Math.PI/180 * 360, true)
-// ctx.arc(50, 50, 20, 0, .5, true)
 ctx.fillStyle = 'gold'
-ctx.fill();
+ctx.fill()
 ctx.closePath()
+
+ctx.beginPath();
+ctx.arc(20, 120, 10, 0, Math.PI*2)
+ctx.strokeStyle = "pink"
+ctx.lineWidth = 10
+ctx.stroke()
+ctx.closePath()
+
+
+
 // Notes
 // 1 radian = 57.2957795 degrees
 // var radians = (Math.PI/180) * degrees
